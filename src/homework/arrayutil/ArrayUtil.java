41f33d3ee29ccf4e30e9a2ddc761a65f128cbcb7
sargis mkrtchyan <mkrtchyansargis395@gmail.com>
2022-05-15 15:55:57 +0400

    Arrayutil- largest elements

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -2,11 +2,16 @@ package homework.arrayutil;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int i = numbers.length - 1;
-        System.out.println(numbers[i]);
-    }
+        int[] nums = new int[]{1, 45, 3, 4, 5, 6, 9, 5, 8};
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>max) {
+                max= nums[i] ;
+            }
 
+        }
+        System.out.println(max);
+    }
 }
 
 
