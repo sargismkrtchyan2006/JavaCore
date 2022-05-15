c2f8566dcfb4ab33d173c6d1bc0c67d4f7fe9270
sargis mkrtchyan <mkrtchyansargis395@gmail.com>
2022-05-15 13:04:38 +0400

    Arrayutil-smallest element

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -3,12 +3,12 @@ package homework.arrayutil;
 public class ArrayUtil {
     public static void main(String[] args) {
         int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int max = numbers[0];
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > max) ;
-            max = numbers[i];
+        int min = numbers[0];
+        for (int i = 0; i > numbers.length; i--) {
+            if (numbers[i] > min) ;
+            min = numbers[i];
         }
-        System.out.print("largest element=" + max);
+        System.out.print("smallest element=" + min);
     }
 
 }
