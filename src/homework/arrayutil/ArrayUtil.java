cb2b6774e6e00cd6e049481ca83818927813bfe0
sargis mkrtchyan <mkrtchyansargis395@gmail.com>
2022-05-15 22:34:27 +0400

    Arrayutil- pair elements

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -23,7 +23,7 @@ public class ArrayUtil {
         int[] nums = {1, 45, 3, 4, 5, 6, 9, 64, 8, 91};
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 1) {
+            if (nums[i] % 2 == 0) {
                 System.out.print(nums[i]);
                 System.out.print(" ");
 
