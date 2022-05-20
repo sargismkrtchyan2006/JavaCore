a2448792459f29867290c326e2beee89f9481f17
sargis mkrtchyan <mkrtchyansargis395@gmail.com>
2022-05-20 16:30:00 +0400

    Chararrayexample

--- src/homework/arrayutil/CharArrayExample.java
+++ src/homework/arrayutil/CharArrayExample.java
@@ -14,25 +14,25 @@ public class CharArrayExample {
 //        }
 //
 //        System.out.println(count);
-              char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
-        char first;
-        char seconds;
-        first = (char) (chars2.length / 2);
-        seconds = (char) (chars2.length / 2 - 1);
+//              char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+//        char first;
+//        char seconds;
+//        first = (char) (chars2.length / 2);
+//        seconds = (char) (chars2.length / 2 - 1);
+//
+//
+//        System.out.println(chars2[first] + " " + chars2[seconds]);
+
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
 
 
-        System.out.println(chars2[first] + " " + chars2[seconds]);
+        if (chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l') {
 
-//        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
-//
-//
-//        if (chars3[chars3.length-1] == 'y' && chars3[chars3.length-2] == 'l') {
-//
-//            System.out.println(true);
-//        } else {
-//            System.out.println(false);
-//
-//        }
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+
+        }
 //        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
 //        for (int i = 0; i < text.length; i++) {
 //            if (text[i] != ' '){
