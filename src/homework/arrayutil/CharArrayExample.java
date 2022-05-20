a758887e3814d0291642e954a0138b0df4674c18
sargis mkrtchyan <mkrtchyansargis395@gmail.com>
2022-05-20 16:31:11 +0400

    Chararrayexample

--- src/homework/arrayutil/CharArrayExample.java
+++ src/homework/arrayutil/CharArrayExample.java
@@ -23,22 +23,22 @@ public class CharArrayExample {
 //
 //        System.out.println(chars2[first] + " " + chars2[seconds]);
 
-        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
-
-
-        if (chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l') {
-
-            System.out.println(true);
-        } else {
-            System.out.println(false);
-
-        }
-//        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
-//        for (int i = 0; i < text.length; i++) {
-//            if (text[i] != ' '){
-//                System.out.print(text[i ]);
-//            }
+//        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
+//
+//
+//        if (chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l') {
+//
+//            System.out.println(true);
+//        } else {
+//            System.out.println(false);
+//
 //        }
+        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] != ' '){
+                System.out.print(text[i ]);
+            }
+        }
 //        char[] bobarray = {'b', 'a', 'b', 'o', 'l', 'a'};
 //        for (int i = 0; i < bobarray.length; i++) {
 //            if (bobarray[i]=='b'&&bobarray[i+2]=='b'){
