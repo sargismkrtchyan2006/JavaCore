e3d395cd44b8c76c3e3300bdc3381f0730155d0c
sargis mkrtchyan <mkrtchyansargis395@gmail.com>
2022-06-05 15:46:45 +0400

    PracticeHomeworkDemo

--- src/homework/PracticeHomework.java
+++ src/homework/PracticeHomework.java
@@ -1,36 +1,34 @@
 package homework;
 
 public class PracticeHomework {
-    long convert(int minutes, int seconds) {
-        return (minutes * seconds);
+    long convert(int minutes) {
+        return (minutes * 60);
 
     }
 
-    int calcage(int years, int day) {
-        return (years * day);
+    int calcage(int years) {
+        return (years * 365);
     }
 
     int nextnumber(int number) {
-        int nextnumber = number + 1;
-        return (nextnumber);
+        return (++number);
     }
 
     boolean isSameNum(int a, int b) {
-        if (a == b) {
-            return (true);
-        } else {
-            return (false);
-        }
+        return a == b;
     }
 
     boolean lessThanOrEqualToZero(int number) {
-        if (number <= 0) {
-            return (true);
-        } else {
-            return (false);
-        }
+        return number <= 0;
     }
 
+    boolean reverseboolean(boolean value) {
+        return !value;
+    }
 
-
+    int maxlength(int[] array1, int[] array2) {
+        int array1length = array1.length;
+        int array2length = array2.length;
+        return array1.length > array2.length ? array1.length : array2.length;
+    }
 }
--- src/homework/PracticeHomeworkDemo.java
+++ src/homework/PracticeHomeworkDemo.java
@@ -3,11 +3,14 @@ package homework;
 public class PracticeHomeworkDemo {
     public static void main(String[] args) {
         PracticeHomework practiceHomework = new PracticeHomework();
-        System.out.println(practiceHomework.convert(3, 60));
-        System.out.println(practiceHomework.calcage(5, 365));
-        System.out.println(practiceHomework.nextnumber(160));
-        System.out.println(practiceHomework.isSameNum(6, 6));
+        System.out.println(practiceHomework.convert(15));
+        System.out.println(practiceHomework.calcage(9));
+        System.out.println(practiceHomework.nextnumber(9));
+        System.out.println(practiceHomework.isSameNum(6,6));
         System.out.println(practiceHomework.lessThanOrEqualToZero(9));
-        System.out.println();
+        System.out.println(practiceHomework.reverseboolean(true));
+        int[]array1={2,6,9};
+        int[]array2={6,9,4,7};
+        System.out.println(practiceHomework.maxlength(array1,array2));
     }
 }
